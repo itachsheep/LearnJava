4f52a534c3a6570e4219a3c88c3be0459c83180e
taow <taow@inspur.com>
2017-05-26 10:19:17 +0800

    create Player demo

---
+++ SelfView/app/src/main/java/andorid/taow/selfview/AutoTextViewActivity.java
@@ -0,0 +1,13 @@
+package andorid.taow.selfview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AutoTextViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
