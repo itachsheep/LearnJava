5cc7e59dedfe1099e111e0df1e901e9c1577bf6d
taow <taow@inspur.com>
2017-07-20 00:12:55 +0800

    add permutation algorithm

---
+++ Algorithm/lib/src/main/java/structe_data/Recursion_1_Triangle.java
@@ -0,0 +1,28 @@
+package structe_data;
+
+/**
+ * Created by taowei on 2017/7/19.
+ * 2017-07-19 22:29
+ * Algorithm
+ * structe_data
+ */
+
+public class Recursion_1_Triangle {
+    public static void main(String[] args){
+        for(int i = 1; i < 10; i++){
+            System.out.println(i+" ---------- "+triangle(i));
+        }
+    }
+
+    public static int triangle(int n){
+        /**
+         * 1  1
+         * 3  1+2
+         * 6  1+2+3
+         * ...
+         * n
+         */
+        if(n == 1) return 1;
+        return triangle(n-1)+n;
+    }
+}
