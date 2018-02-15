178dba8af1072a7bdda03f4811c9ba1c0b34a06f
itachsheep <itachsheep@163.com>
2018-02-15 09:42:05 +0800

    leetcode 10, regualr express matching

---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_10_regular_express_matching.java
@@ -0,0 +1,47 @@
+package com.tao.javacode;
+
+/**
+ * Created by taowei on 2018/2/15.
+ * 2018-02-15 07:15
+ * leetcode
+ * com.tao.javacode
+ */
+
+
+/**
+ * Implement regular expression matching with support for '.' and '*'.
+
+ '.' Matches any single character.
+ '*' Matches zero or more of the preceding element.
+
+ The matching should cover the entire input string (not partial).
+
+ The function prototype should be:
+ bool isMatch(const char *s, const char *p)
+
+ Some examples:
+ isMatch("aa","a") → false
+ isMatch("aa","aa") → true
+ isMatch("aaa","aa") → false
+ isMatch("aa", "a*") → true
+ isMatch("aa", ".*") → true
+ isMatch("ab", ".*") → true
+ isMatch("aab", "c*a*b") → true
+ */
+public class _10_regular_express_matching {
+    public static void main(String[] args){
+        System.out.println(isMatch("aab","c*a*b"));
+    }
+    public static boolean isMatch(String text, String pattern) {
+        if (pattern.isEmpty()) return text.isEmpty();
+        boolean first_match = (!text.isEmpty() &&
+                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*'){
+            return (isMatch(text, pattern.substring(2)) ||
+                    (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+}
