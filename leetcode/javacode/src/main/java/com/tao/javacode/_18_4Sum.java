7809013a9ebcaacb568c25ecc931986d6deee984
itachsheep <itachsheep@163.com>
2018-03-29 09:10:06 +0800

    leetcode 18,数组中找出和为某个数的四个数

---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_18_4Sum.java
@@ -0,0 +1,23 @@
+package com.tao.javacode;
+
+/**
+ *
+ * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+ * Find all unique quadruplets in the array which gives the sum of target.
+ *
+ * For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+
+ A solution set is:
+ [
+ [-1,  0, 0, 1],
+ [-2, -1, 1, 2],
+ [-2,  0, 0, 2]
+ ]
+ * Created by SDT14324 on 2018/3/29.
+ */
+
+public class _18_4Sum {
+    public static void main(String[] args){
+
+    }
+}
---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_19_remove_nth_node_from_list_end.java
@@ -0,0 +1,11 @@
+package com.tao.javacode;
+
+/**
+ * Created by SDT14324 on 2018/3/29.
+ */
+
+public class _19_remove_nth_node_from_list_end {
+    public static void main(String[] args){
+
+    }
+}
