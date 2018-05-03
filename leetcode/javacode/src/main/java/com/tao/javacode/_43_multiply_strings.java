75a7672be8670b1ae82aefc8ceb326bc67011a76
itachsheep <itachsheep@163.com>
2018-05-03 22:05:12 +0800

    leetcode 43,字符串数字做乘法

---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_43_multiply_strings.java
@@ -0,0 +1,49 @@
+package com.tao.javacode;
+
+/**
+ *
+ * Given two non-negative integers num1 and num2 represented as strings,
+ * return the product of num1 and num2, also represented as a string.
+
+ Example 1:
+
+ Input: num1 = "2", num2 = "3"
+ Output: "6"
+ Example 2:
+
+ Input: num1 = "123", num2 = "456"
+ Output: "56088"
+ Note:
+
+ The length of both num1 and num2 is < 110.
+ Both num1 and num2 contain only digits 0-9.
+ Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+ You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+ * Created by SDT14324 on 2018/5/3.
+ */
+
+public class _43_multiply_strings {
+    public static void main(String[] args){
+        String uuid = "QD406-000000000000000000";
+        System.out.println(uuid.substring(6));
+    }
+
+    //`num1[i] * num2[j]` will be placed at indices `[i + j`, `i + j + 1]`
+    public static String multiply(String num1, String num2) {
+        int m = num1.length(),n = num2.length();
+        int[] pos = new int[m+n];
+        for (int i = m - 1;i >= 0 ;i--){
+            for (int j = n -1; j >= 0; j--){
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i+j,p2 = i+j+1;
+                int sum = mul + pos[p2];
+                pos[p1] += sum/10;
+                pos[p2] = sum%10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+//        for (int )
+        return sb.toString();
+    }
+}
