3a8922e12f6b0336eca6ef2208716a63d7932aae
itachsheep <itachsheep@163.com>
2018-05-04 09:28:50 +0800

    leetcode 44, 字符串匹配

---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_44_wildcard_match.java
@@ -0,0 +1,84 @@
+package com.tao.javacode;
+
+/**
+ *
+ * Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+ '?' Matches any single character.
+ '*' Matches any sequence of characters (including the empty sequence).
+ The matching should cover the entire input string (not partial).
+
+ Note:
+
+ s could be empty and contains only lowercase letters a-z.
+ p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+ Example 1:
+
+ Input:
+ s = "aa"
+ p = "a"
+ Output: false
+ Explanation: "a" does not match the entire string "aa".
+ Example 2:
+
+ Input:
+ s = "aa"
+ p = "*"
+ Output: true
+ Explanation: '*' matches any sequence.
+ Example 3:
+
+ Input:
+ s = "cb"
+ p = "?a"
+ Output: false
+ Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+ Example 4:
+
+ Input:
+ s = "adceb"
+ p = "*a*b"
+ Output: true
+ Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+ Example 5:
+
+ Input:
+ s = "acdcb"
+ p = "a*c?b"
+ Output: false
+ * Created by SDT14324 on 2018/5/4.
+ */
+
+public class _44_wildcard_match {
+    public static void main(String[] args){
+//        System.out.println("res = "+isMatch("aa","a"));
+//        System.out.println("res = "+isMatch("aa","*"));
+//        System.out.println("res = "+isMatch("cb","?a"));
+        System.out.println("res = "+isMatch("adceb","*a*b"));
+//        System.out.println("res = "+isMatch("acdcb","a*c?b"));
+    }
+
+    public static  boolean isMatch(String str, String pattern) {
+        int s = 0, p = 0, match = 0, starIdx = -1;
+        while ( s < str.length()){
+            if(p < pattern.length() && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
+                s++;
+                p++;
+            }else if(p < pattern.length() && pattern.charAt(p) == '*'){
+                starIdx = p;
+                match = s;
+                p++;
+            }else if(starIdx != -1){
+                p = starIdx + 1;
+                match++;
+                s = match;
+            }else {
+                return false;
+            }
+        }
+        while (p < pattern.length() && pattern.charAt(p) == '*'){
+            p++;
+        }
+        return p == pattern.length();
+    }
+}
