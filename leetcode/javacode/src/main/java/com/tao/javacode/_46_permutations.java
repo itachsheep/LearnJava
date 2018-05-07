2260f8eda8fc42ba40e04ff7a118b201340aff67
itachsheep <itachsheep@163.com>
2018-05-07 21:35:26 +0800

    leetcode 46,排列

---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_46_permutations.java
@@ -0,0 +1,16 @@
+package com.tao.javacode;
+
+import java.util.List;
+
+/**
+ * Created by SDT14324 on 2018/5/7.
+ */
+
+public class _46_permutations {
+    public static void main(String[] args){
+
+    }
+    public static List<List<Integer>> permute(int[] nums) {
+
+    }
+}
