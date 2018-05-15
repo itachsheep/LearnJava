e7a1ce8598dc8df88bf1c7bea4c0fff433d9206a
itachsheep <itachsheep@163.com>
2018-05-15 19:28:43 +0800

    leetcode 53，求一个数组中最大的和的值

---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_51_n_queens.java
@@ -0,0 +1,40 @@
+package com.tao.javacode;
+
+import java.util.List;
+
+/**
+ * The n-queens puzzle is the problem of placing n queens on an n×n chessboard
+ * such that no two queens attack each other.
+ Given an integer n, return all distinct solutions to the n-queens puzzle.
+
+ Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' b
+ oth indicate a queen and an empty space respectively.
+
+ Example:
+
+ Input: 4
+ Output: [
+ [".Q..",  // Solution 1
+ "...Q",
+ "Q...",
+ "..Q."],
+
+ ["..Q.",  // Solution 2
+ "Q...",
+ "...Q",
+ ".Q.."]
+ ]
+ Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+
+ * Created by SDT14324 on 2018/5/15.
+ */
+
+public class _51_n_queens {
+    public static void main(String[] args){
+
+    }
+
+    public static List<List<String>> solveNQueens(int n) {
+        return null;
+    }
+}
---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_53_maxmum_subarray.java
@@ -0,0 +1,32 @@
+package com.tao.javacode;
+
+/**
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at least one number)
+ * which has the largest sum and return its sum.
+
+ Example:
+
+ Input: [-2,1,-3,4,-1,2,1,-5,4],
+ Output: 6
+ Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Created by SDT14324 on 2018/5/15.
+ */
+
+public class _53_maxmum_subarray {
+    public static void main(String[] args){
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < n; i++){
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i-1] : 0);
+            max = Math.max(max,dp[i]);
+        }
+        return max;
+    }
+}
