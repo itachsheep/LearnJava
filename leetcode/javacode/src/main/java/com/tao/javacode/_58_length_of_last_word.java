b105ff4307071d10df4334aa8455afe8f0be492b
itachsheep <itachsheep@163.com>
2018-05-25 10:40:43 +0800

    leetcode 59, 旋涡矩阵构造

--- leetcode/javacode/src/main/java/com/tao/javacode/_58_length_of_last_word.java
+++ leetcode/javacode/src/main/java/com/tao/javacode/_58_length_of_last_word.java
@@ -2,7 +2,8 @@ package com.tao.javacode;
 
 /**
  *
- * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+ * return the length of last word in the string.
 
  If the last word does not exist, return 0.
 
---
+++ leetcode/javacode/src/main/java/com/tao/javacode/_59_spiral_matrix.java
@@ -0,0 +1,31 @@
+package com.tao.javacode;
+
+/**
+ * Created by taowei on 2018/5/25.
+ *
+ * Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+ Example:
+
+ Input: 3
+ Output:
+ [
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+ ]
+
+ * 2018-05-25 10:18
+ * leetcode
+ * com.tao.javacode
+ */
+
+public class _59_spiral_matrix {
+    public static void main(String[] args){
+
+    }
+
+    public static int[][] generateMatrix(int n) {
+
+    }
+}
