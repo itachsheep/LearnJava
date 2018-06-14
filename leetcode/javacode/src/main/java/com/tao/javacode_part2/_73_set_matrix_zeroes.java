7412c6b2644a6400f0956682ca12c06c937c756f
itachsheep <itachsheep@163.com>
2018-06-14 08:42:18 +0800

    leetcode 73 将矩阵二维数组设置为0

---
+++ leetcode/javacode/src/main/java/com/tao/javacode_part2/_73_set_matrix_zeroes.java
@@ -0,0 +1,66 @@
+package com.tao.javacode_part2;
+
+/**
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+
+ Example 1:
+
+ Input:
+ [
+ [1,1,1],
+ [1,0,1],
+ [1,1,1]
+ ]
+ Output:
+ [
+ [1,0,1],
+ [0,0,0],
+ [1,0,1]
+ ]
+ Example 2:
+
+ Input:
+ [
+ [0,1,2,0],
+ [3,4,5,2],
+ [1,3,1,5]
+ ]
+ Output:
+ [
+ [0,0,0,0],
+ [0,4,5,0],
+ [0,3,1,0]
+ ]
+ Follow up:
+
+ A straight forward solution using O(mn) space is probably a bad idea.
+ A simple improvement uses O(m + n) space, but still not the best solution.
+ Could you devise a constant space solution?
+
+ */
+public class _73_set_matrix_zeroes {
+    public static void main(String[] args){
+        //二维数组：第一个元素代表行，第二个元素代表列，a[rows][columns]
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+        for (int i = 0; i < rows; i++){//一行一行遍历
+            if(matrix[i][0] == 0) col0 = 0;//第一列如果为0，标记col0
+            for (int j = 1; j < cols; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j] = 0; //[i,j]为0，则第一行的j列元素置为0，第一列的i行元素置为0
+                }
+            }
+        }
+
+        for(int i = rows - 1; i >= 0; i--){
+            for (int j = cols -1 ; j >= 1; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+            if(col0 == 0) matrix[i][0] = 0;
+        }
+    }
+}
