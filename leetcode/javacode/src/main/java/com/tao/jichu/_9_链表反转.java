6b86a3a78b4b3deefb06ed42e92093d4ad092589
itachsheep <itachsheep@163.com>
2019-03-05 00:09:26 +0800

    基础 链表翻转验证ok

--- leetcode/javacode/src/main/java/com/tao/jichu/_9_链表反转.java
+++ leetcode/javacode/src/main/java/com/tao/jichu/_9_链表反转.java
@@ -1,9 +1,18 @@
 package com.tao.jichu;
 
 import com.tao.comm.ListNode;
+import com.tao.comm.Utils;
 
 public class _9_链表反转 {
-    public ListNode reverse(ListNode head){
+
+    public static void main(String[] args){
+        ListNode head = Utils.createList(new int[]{1, 2, 3, 4, 5});
+        Utils.printList(head);
+        ListNode reverse = reverse(head);
+        System.out.println("--------------------------");
+        Utils.printList(reverse);
+    }
+    public static ListNode reverse(ListNode head){
         if(head == null || head.next == null) return head;
         ListNode p = head.next;
         head.next = null;
