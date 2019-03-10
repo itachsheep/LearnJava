95705bfb5e9b06e58723c4e71fcf89d7257f1fa6
itachsheep <itachsheep@163.com>
2019-03-10 23:57:45 +0800

    leetcode top1oo 回文子串

---
+++ leetcode/javacode/src/main/java/com/tao/top_100_liked_questions/_647_Palindromic_Substrings.java
@@ -0,0 +1,26 @@
+package com.tao.top_100_liked_questions;
+
+/**
+ * Given a string, your task is to count how many palindromic substrings in this string.
+ *
+ * The substrings with different start indexes or end indexes are counted as different
+ * substrings even they consist of same characters.
+ *
+ * Example 1:
+ *
+ * Input: "abc"
+ * Output: 3
+ * Explanation: Three palindromic strings: "a", "b", "c".
+ *
+ *
+ * Example 2:
+ *
+ * Input: "aaa"
+ * Output: 6
+ * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ */
+public class _647_Palindromic_Substrings {
+    public int countSubstrings(String s) {
+
+    }
+}
