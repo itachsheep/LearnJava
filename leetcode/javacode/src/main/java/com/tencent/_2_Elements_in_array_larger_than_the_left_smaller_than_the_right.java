f9140c81192c271a1567d3d1246e61a82920be13
itachsheep <itachsheep@163.com>
2020-05-03 16:15:42 +0800

    字符串去掉空格

--- leetcode/javacode/src/main/java/com/tencent/_2_Elements_in_array_larger_than_the_left_smaller_than_the_right.java
+++ leetcode/javacode/src/main/java/com/tencent/_2_Elements_in_array_larger_than_the_left_smaller_than_the_right.java
@@ -29,6 +29,16 @@ public class _2_Elements_in_array_larger_than_the_left_smaller_than_the_right {
 
     }
 
+    /**
+     * https://52heartz.top/articles/algorithm-find-elements-before-which-all-the-elements-are-smaller-than-it-and-after-which-all-are-greater/
+     *
+     * 一个数要比它左边的所有数要大，比右边的所有数要小，那么它必定大于左边元素的最大值，同时小于右边元素的最小值。
+     *
+     * 两次遍历。第一次遍历从后向前，找出第 i 个元素右边元素的最小值，保存在 rightMin 数组中。
+     * 第二次遍历，从前向后，使用一个临时变量保存左边元素的最大值。一边判断一边更新。
+     * @param nums
+     * @return
+     */
     public static List<Integer> findElements(int[] nums){
         List<Integer> output = new ArrayList<>();
         int[] rightMins = new int[nums.length];
