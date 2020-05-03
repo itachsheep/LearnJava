9f0a8e67891658cf80c9426555614265fdd5bb35
itachsheep <itachsheep@163.com>
2020-05-03 16:49:49 +0800

    链表反转

--- leetcode/javacode/src/main/java/com/Util.java
+++ leetcode/javacode/src/main/java/com/Util.java
@@ -1,5 +1,7 @@
 package com;
 
+import com.tao.comm.ListNode;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -28,5 +30,29 @@ public class Util<T> {
             System.out.print(list.get(i) + " ");
         }
         System.out.println();
+    }
+
+    public void printList(ListNode head){
+        if(head == null)return;
+        while (head != null){
+            System.out.print(head.val +" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public ListNode createList(int[] num){
+        ListNode head = null, tail;
+        if(num == null || num.length <=0 ) return null;
+        if(num != null && num.length >= 1) {
+            head = new ListNode(num[0]);
+        }
+        tail = head;
+        for(int i = 1; i < num.length; i++){
+            ListNode node = new ListNode(num[i]);
+            tail.next = node;
+            tail = node;
+        }
+        return head;
     }
 }
---
+++ leetcode/javacode/src/main/java/com/tencent/_4_linked_list_reversal.java
@@ -0,0 +1,31 @@
+package com.tencent;
+
+import com.Util;
+import com.tao.comm.ListNode;
+
+public class _4_linked_list_reversal {
+    public static void main(String[] args) {
+       Util<Integer> util = new Util<Integer>();
+       int[] num = {1,2,3,4,5,6,7,8,9};
+        ListNode head = util.createList(num);
+        util.printList(head);
+        ListNode reverse = reverseList(head);
+        util.printList(reverse);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+
+        ListNode preListNode = null;
+        ListNode curListNode = head;
+        while (curListNode != null) {
+            ListNode nextListNode = curListNode.next;
+            curListNode.next = preListNode;
+            preListNode = curListNode;
+            curListNode = nextListNode;
+        }
+        return preListNode;
+    }
+}
