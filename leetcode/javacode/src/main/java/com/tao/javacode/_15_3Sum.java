5f5f3d273358e0f78c024f5e178d827b6a594f65
itachsheep <itachsheep@163.com>
2020-05-05 17:04:31 +0800

    数组中和为sum的组合

--- leetcode/javacode/src/main/java/com/tao/javacode/_15_3Sum.java
+++ leetcode/javacode/src/main/java/com/tao/javacode/_15_3Sum.java
@@ -6,10 +6,11 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ Given an array S of n integers, are there elements a, b, c in S such
+ that a + b + c = 0? Find all unique triplets in the array which gives
+ the sum of zero.
 
  Note: The solution set must not contain duplicate triplets.
-
  For example, given array S = [-1, 0, 1, 2, -1, -4],
 
  A solution set is:
--- leetcode/javacode/src/main/java/com/tao/javacode/_18_4Sum.java
+++ leetcode/javacode/src/main/java/com/tao/javacode/_18_4Sum.java
@@ -2,7 +2,8 @@ package com.tao.javacode;
 
 /**
  *
- * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+ * Given an array S of n integers, are there elements a, b, c,and d in S
+ * such that a + b + c + d = target?
  * Find all unique quadruplets in the array which gives the sum of target.
  *
  * For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
